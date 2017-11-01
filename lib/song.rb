9e611cd9b79aa191d412ad493e871b0d97b9dc7f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-01 07:59:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,11 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@artists = []
+  @@genres = []
+  @@artist_count = {}
+  @@genre_count = {}
   def initialize(name,artist,genre)
+    
+  end
 end
