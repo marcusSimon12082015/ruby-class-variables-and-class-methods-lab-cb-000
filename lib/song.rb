3a58390ae684f6b080e8b91aa99b04216a2b56e3
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-01 08:07:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,25 @@ class Song
   @@artist_count = {}
   @@genre_count = {}
   def initialize(name,artist,genre)
-    
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    if !@@artists.include?(artist)
+      @@artists << artist
+    end
+    if !@@genres.include?(genre)
+      @@genre << genre
+    end
+    if @@artist_count.key?(artist)
+      @@artist_count[artist] =+1
+    else
+      @@artist_count[artist] = 0
+    end
+    if @@genre_count.key?(genre)
+      @@genre_count[genre] =+1
+    else
+      @@genre_count[genre] = 0
+    end
   end
 end
